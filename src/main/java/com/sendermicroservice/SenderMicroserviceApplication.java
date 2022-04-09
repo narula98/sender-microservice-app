156bf29d198f1579a336b05de89147ef6941293d
narula98 <sahilnarula98@gmaill.com>
2022-04-09 18:28:03 +0530

    Initialized spring project and git repo for sender microservice

---
+++ src/main/java/com/sendermicroservice/SenderMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sendermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sendermicroservice/SenderMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
